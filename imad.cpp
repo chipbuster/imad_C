7d7151997c9843c8f137679c826e9c8047a45b88
Kevin Song <chipbuster@gmail.com>
2014-07-13 14:12:00 -0700

    Modified gitignore and fleshed out main algorithm
    Gitignore has been updated. imad.cpp has been further written and now
    reads in the images to the tile buffer (in theory).

--- imad.cpp
+++ imad.cpp
@@ -5,7 +5,11 @@
 #include <vector>
 #include <Eigen/Dense>
 
-void imad(std::string filename1="", std::string filename2=""){
+using namespace std;
+using Eigen::MatrixXf;
+using Eigen::VectorXf;
+
+void imad(string filename1="", string filename2=""){
   GDALAllRegister(); //Must be called before GDAL functions
 
   //openFile() will ask us for filenames if the names are blank
@@ -17,22 +21,74 @@ void imad(std::string filename1="", std::string filename2=""){
   int nrow = file1->GetRasterXSize();
   int ncol = file1->GetRasterYSize();
 
-  //Temporary hardcoding. Eventually, offsets will be user-specified
+  //Temporary hardcoding. Eventually, will be user-specified
   int xoffset = 0;
   int yoffset = 0;
+  int maxiter = 10;
+  double tolerance = 0.001;
 
-  std::vector<int>& bandnums = *GdalFileIO::selectBands();
+  /* selectBands() gets a series of bands from the user. For example, for
+  a LandSat7 image, the vector might return [1,2,3,4,5,7] */
+  vector<int>& bandnums = *GdalFileIO::selectBands();
   int nBands = bandnums.size();
 
-//   GDALRasterBand& bands_1 = new GDALRasterBand[bandnums.size()];
-//   GDALRasterBand& bands_2 = new GDALRasterBand[bandnums.size()];
-//
-// //Get Raster band objects and place in array
-//   for(size_t i = 0; i < bandnums.size(); i++){
-//     bands_1[i] = file1.GetRasterBand(bandnums[i]);
-//     bands_2[i] = file2.GetRasterBand(bandnums[i]);
-//   }
+  vector<GDALRasterBand*> bands_1 = vector<GDALRasterBand*>(nBands);
+  vector<GDALRasterBand*> bands_2 = vector<GDALRasterBand*>(nBands);
+  vector<double> noDataValues      = vector<double>(nBands);
 
+  //Get easy handles to raster bands (pointers)
+  for(size_t i = 0; i < bandnums.size(); i++){
+    bands_1[i]     = (file1->GetRasterBand(bandnums[i]));
+    bands_2[i]     = (file1->GetRasterBand(bandnums[i]));
+    noDataValues[i] = bands_1[i]->GetNoDataValue();
+  }
+
+  //Input state is done. Set up for calculations.
+
+  /* tile is a strange name for a matrix (borrowed from the python code).
+  It holds a single row of both images. The top half of the matrix holds
+  the row from file1, with all of its bands in separate rows (e.g. for any
+  given row, row 1 of tile will hold image1, band1, row 2 will hold image1,
+  band2, etc.). The same is true of the second image in the bottom half of the
+  matrix. */
+
+  float* tile = new float[2 * nBands * ncol];
+  ImageStats cpm = ImageStats(nBands * 2);
+  double delta = 1.0;
+  VectorXf rho, oldrho;
+  MatrixXf sigMADs, means1, means2, A, B, cov;
+
+  //Start calculations (note double conditional in for-loop)
+  for(int iter = 0; iter < maxiter && delta > tolerance; iter++){
+    for(int row = 0; row < nrow; row++){
+      for(int k = 0; k < nBands; k++){
+        //Read into the appropriate location in tile.
+        //The Python code for this might be easier to understand.
+        bands_1[k]->RasterIO(GF_Read, xoffset, yoffset, ncol, 1,
+                             &tile[k * ncol], ncol, 1,
+                             GDT_Float32, 0,0 );
+        bands_1[k]->RasterIO(GF_Read, xoffset, yoffset, ncol, 1,
+                             &tile[(k + nBands) * ncol], ncol, 1,
+                             GDT_Float32, 0,0 );
+      }
+      if(iter > 0){} //Not sure how to implement this yet
+      else cpm.update(tile, NULL, ncol, 2*nBands);
+    }
+    MatrixXf S = cpm.get_covar();
+    VectorXf means = cpm.get_means();
+    MatrixXf s11, s12, s21, s22, b1, c1, b2, c2;
+    
+
+
+
+
+
+
+
+  }
+
+  delete &bandnums;
+  delete[] tile;
 }
 
 int main(){ //dummy main
