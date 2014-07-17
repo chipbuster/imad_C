d3492ba1defe842ae5e8a444885a0d1cdf7d6116
Kevin Song <chipbuster@gmail.com>
2014-07-16 18:04:36 -0700

    Transformation systems primed!
    Finished writing a prototype coordinate transformation "CoordTransform"
    Not tested yet. Please do not use until ready

--- GdalFileIO.cpp
+++ GdalFileIO.cpp
@@ -1,9 +1,11 @@
 #include "gdal_priv.h"
+#include "imad.h"
 #include <vector>
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include <stdexcept>
+#include <math.h>
 
 using namespace std;
 
@@ -26,6 +28,8 @@ namespace GdalFileIO{
       return dataset1;
     }
 
+    /*************************************************************************/
+
     bool dimensionsmatch(GDALDataset* dataset1, GDALDataset* dataset2){
       /* Check that dimensions of the two files match in X, Y, and num. bands */
       if(dataset1->GetRasterXSize() != dataset2->GetRasterXSize()){
@@ -44,6 +48,8 @@ namespace GdalFileIO{
       return true;
     }
 
+    /*************************************************************************/
+
     vector<int>* selectBands(){
       vector<int>* bands = new vector<int>();
       cout << "Please enter the band numbers you would like to use, followed by a return"
@@ -61,5 +67,60 @@ namespace GdalFileIO{
       return bands;
     }
 
-    
+    /*************************************************************************/
+
+    CoordTransform::CoordTransform(double* transform_coeff){
+      //Notes can be found in the header file. Construct affine transform matrix
+      Img2Geo(0,2) = transform_coeff[0];
+      Img2Geo(1,2) = transform_coeff[3];
+      Img2Geo(0,0) = transform_coeff[1];
+      Img2Geo(0,1) = transform_coeff[2];
+      Img2Geo(1,0) = transform_coeff[4];
+      Img2Geo(1,1) = transform_coeff[5];
+      Img2Geo(2,0) = 0;
+      Img2Geo(2,1) = 0;
+      Img2Geo(2,2) = 1;
+      Geo2Img = Img2Geo.inverse();
+
+      //Last element of affine transform vectors is always 1
+      input(2) = 1;
+    }
+
+    double CoordTransform::ImgtoGeo_X(double imgP, double imgL){
+      input(0) = imgP;
+      input(1) = imgL;
+      input(2) = 1;
+      output = Img2Geo * input;
+      return output(0);
+    }
+    double CoordTransform::ImgtoGeo_Y(double imgP, double imgL){
+      input(0) = imgP;
+      input(1) = imgL;
+      output = Img2Geo * input;
+      return output(1);
+    }
+    double CoordTransform::GeotoImg_X(double geoX, double geoY){
+      input(0) = geoX;
+      input(1) = geoY;
+      output = Geo2Img * input;
+      return round(output(0));
+    }
+    double CoordTransform::GeotoImg_Y(double geoX, double geoY){
+      input(0) = geoX;
+      input(1) = geoY;
+      output = Geo2Img * input;
+      return round(output(1));
+    }
+    void CoordTransform::GeotoImg(double& X, double& Y){
+      double tmpX = GeotoImg_X(X,Y);
+      double tmpY = GeotoImg_Y(X,Y);
+      X = tmpX; Y = tmpY;
+    }
+    void CoordTransform::ImgtoGeo(double& X, double& Y){
+      double tmpX = ImgtoGeo_X(X,Y);
+      double tmpY = ImgtoGeo_Y(X,Y);
+      X = tmpX; Y = tmpY;
+    }
+
+    /*************************************************************************/
 }
--- imad.h
+++ imad.h
@@ -16,30 +16,40 @@ using namespace Eigen;
 /* This file contains the headers for the iMad project. */
 /* All GDAL functions derive from gdal_priv.h           */
 /* Namespaces are named after their .cpp files, e.g. all functions in namespace
- * GdalFileIO are implemented in GdalFileIO.cpp */
+ * GdalFileIO are implemented in GdalFileIO.cpp. */
 
 typedef Map<Matrix<float,Dynamic,Dynamic,RowMajor> > MapRMMatrixXf;
 
 namespace GdalFileIO{
 
-/*
-  struct ImageDims{
-    int xoffset;
-    int yoffset;
-    int nrow;
-    int ncol;
+  struct CoordTransform{
+    /* Takes the Geotransform of an image and uses them to transform coordinate
+     * Only one CoordTransform object per image projection, please. */
+    //Look up affine transforms for more details on implementation.
+    /* in the transform constants, X,Y refer to geospatial coordinates, while
+     * P,L refer to the raster (x,y) coordinates. Raster coordinates start
+     * in the top left and increase going right and down, so a pixel at
+     * (P,L) = (2,5) is in the second column and fifth row of that image.
+     * A derivative (e.g. dA/dB) is denoted as dA_dB in these names.
+     * See docs for GDAL's GetGeoTransform() for more information. */
+     Matrix3d Img2Geo;
+     Matrix3d Geo2Img;
+     Vector3d  input;
+     Vector3d  output;
 
-    bool operator==(const ImageDims& other) const{
-      if(xoffset == other.xoffset &&
-         yoffset == other.yoffset &&
-         nrow    == other.nrow    &&
-         ncol    == other.ncol ){
-           return true;
-         }
-      else return false;
-    }
+     //Needed for vectorizable ops
+     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    CoordTransform(double* transform_coeff);
+    double ImgtoGeo_X(double imgX, double imgY);
+    double ImgtoGeo_Y(double imgX, double imgY);
+    double GeotoImg_X(double geoX, double geoY);
+    double GeotoImg_Y(double geoX, double geoY);
+    void GeotoImg(double& imgX, double& imgY);
+    void ImgtoGeo(double& geoX, double& geoY);
+
+
   };
-  */
 
   //Found in openGdalFiles.cpp
   GDALDataset* openFile(std::string filename);
